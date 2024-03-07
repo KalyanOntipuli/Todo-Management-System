6fa24f3121a177befdbf308be9767940d226951d
Kalyanontipuli <kalyanontipuli87654321@gmail.com>
2024-03-07 10:57:46 +0530

    production database change

--- database.py
+++ database.py
@@ -2,14 +2,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
 
-# MYSQL Series
-# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:test1234!@127.0.0.1:3306/todoapp"
-
+SQLALCHEMY_DATABASE_URL = "postgresql://yibbcfcy:nFZJk_0qN6YXYLmcYCc1imh2djpiUh84@floppy.db.elephantsql.com/yibbcfcy"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
+
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
+
+# # MYSQL Series
+# # SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:test1234!@127.0.0.1:3306/todoapp"
+
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
 
 # MYSQL Series
 # engine = create_engine(
